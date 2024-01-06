b5438daf2c7e11b267088403b1c2664046945c22
jdclavijoc <jesusdavidd3@gmail.com>
2024-01-06 10:50:21 -0500

    Agregar archivos de proyecto.

---
+++ InventarySystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventarySystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
